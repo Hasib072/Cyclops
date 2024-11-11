a8f694ffdabdb3050181a23d864908570662f498
hasib72 <hasibrahaman72@gmail.com>
2024-11-11 19:34:09 +0530

    Updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://cyclops-backend-wx30.onrender.com',
         changeOrigin: true,
       },
     },
